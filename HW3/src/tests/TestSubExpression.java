1bdc0c097cccabc72949b394ffbff877bbed8b15
jyc119 <1jordanchin372@gmail.com>
2023-02-09 11:01:16 -0500

    Passed subtract tests because wrong class name. Same for other classes

--- HW3/src/tests/TestSubExpression.java
+++ HW3/src/tests/TestSubExpression.java
@@ -3,6 +3,7 @@ package tests;
 import hw3.AddExpression;
 import hw3.Expression;
 import hw3.IntExpression;
+import hw3.SubExp;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
@@ -17,18 +18,18 @@ public class TestSubExpression {
 
     @Test
     public void testSubExpressionInt() {
-        Expression root = new AddExpression(new IntExpression(30), new IntExpression(10));
+        Expression root = new SubExp(new IntExpression(30), new IntExpression(10));
         assertEquals(20, root.evaluate());
         assertEquals("(30 - 10)", root.emit());
     }
 
     @Test
     public void testSubExpressionComplex() {
-        Expression root = new AddExpression(
-                new AddExpression(new IntExpression(50), new IntExpression(20)),
-                new AddExpression(new IntExpression(60), new IntExpression(40)));
-        assertEquals(100, root.evaluate());
-        assertEquals("((50 - 20) + (60 - 40))", root.emit());
+        Expression root = new SubExp(
+                new SubExp(new IntExpression(50), new IntExpression(20)),
+                new SubExp(new IntExpression(60), new IntExpression(40)));
+        assertEquals(10, root.evaluate());
+        assertEquals("((50 - 20) - (60 - 40))", root.emit());
     }
 
 }
