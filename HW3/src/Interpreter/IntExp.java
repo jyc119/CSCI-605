2b72d0016779b878befa6308258ad5b8d19a77fd
jyc119 <1jordanchin372@gmail.com>
2023-02-08 21:58:29 -0500

    Added methods for intexp

--- HW3/src/Interpreter/IntExp.java
+++ HW3/src/Interpreter/IntExp.java
@@ -1,4 +1,19 @@
 package Interpreter;
 
 public class IntExp implements Expression{
+
+    private int value;
+
+    public IntExp(int value){
+        this.value = value;
+    }
+
+    public int evaluate(){
+        return value;
+    }
+
+    public String emit(){
+        return String.valueOf(value);
+    }
+
 }
