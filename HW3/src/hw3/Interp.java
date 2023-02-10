6827777e8161408b01a7e65c940e463af5acbaa1
jyc119 <1jordanchin372@gmail.com>
2023-02-09 20:32:39 -0500

    updated helper. Incorrect recursion loop

--- HW3/src/hw3/Interp.java
+++ HW3/src/hw3/Interp.java
@@ -1,29 +1,31 @@
 package hw3;
 
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Interp implements Expression{
+public class Interp {
 
     public Expression helper(ArrayList<String> tokenList){
-        if (tokenList.get(0) == "+"){
+        if (tokenList.get(0).equals("+")){
 
-            return new AddExpression(helper(tokenList.subList(1,tokenList.size())), helper(tokenList.subList(2,tokenList.size())));
+            return new AddExpression(helper((ArrayList<String>) tokenList.subList(1,tokenList.size())), helper((ArrayList<String>) tokenList.subList(2,tokenList.size())));
         }
         else if(tokenList.get(0) == "-") {
-            return SubExp(helper(tokenList[1:]),helper(tokenList[2:]));
+            return new SubExp(helper((ArrayList<String>) tokenList.subList(1,tokenList.size())), helper((ArrayList<String>) tokenList.subList(2,tokenList.size())));
         }
         else if(tokenList.get(0) == "*") {
-            return MulExp(helper(tokenList[1:]),helper(tokenList[2:]));
+            return new MulExp(helper((ArrayList<String>) tokenList.subList(1,tokenList.size())), helper((ArrayList<String>) tokenList.subList(2,tokenList.size())));
         }
         else if(tokenList.get(0) == "/") {
-            return DivExp(helper(tokenList[1:]),helper(tokenList[2:]));
+            return new DivExp(helper((ArrayList<String>) tokenList.subList(1,tokenList.size())), helper((ArrayList<String>) tokenList.subList(2,tokenList.size())));
         }
         else if(tokenList.get(0) == "%") {
-            return ModExp(helper(tokenList[1:]),helper(tokenList[2:]));
+            return new ModExp(helper((ArrayList<String>) tokenList.subList(1,tokenList.size())), helper((ArrayList<String>) tokenList.subList(2,tokenList.size())));
         }
-
+        System.out.println(tokenList.get(0));
+        System.out.println("HERE");
         Expression number = new IntExpression(Integer.parseInt(String.valueOf(tokenList.get(0))));
         return number;
     }
@@ -33,17 +35,22 @@ public class Interp implements Expression{
         Scanner interpreter = new Scanner(System.in);
         System.out.println(">");
         String expression = interpreter.nextLine();
-        while(expression != "quit"){
+        while(!Objects.equals(expression, "quit")){
+            expression = expression.replaceAll("\\s", "");
             String[] tokens = expression.split("");
             ArrayList<String> tokenList = new ArrayList<String>
                     (Arrays.asList(tokens));
-
+            System.out.println(tokenList);
+            Expression expression1 = helper(tokenList);
+            System.out.println(expression1.emit());
+            System.out.println(expression1.evaluate());
         }
         System.out.println("Goodbye!");
         return;
     }
 
     public static void main(String[] args){
-
+        Interp test = new Interp();
+        test.readPrefix();
     }
 }
