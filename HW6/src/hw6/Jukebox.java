aa0c5b9b9032e91a0d86f5dbfb010f0a4fef4730
cal3368 <cal3368@rit.edu>
2023-03-03 13:23:22 -0500

    Set some stuff up for HW6

---
+++ HW6/src/hw6/Jukebox.java
@@ -0,0 +1,14 @@
+package hw6;
+
+public class Jukebox {
+
+
+
+    public static void main(String args[]) {
+        if (args.length != 2) {
+            System.out.println("Usage: java Jukebox filename seed");
+        } else {
+
+        }
+    }
+}
