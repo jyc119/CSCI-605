f98e568048a3a4784fd88e044cc5aade48020480
cal3368 <cal3368@rit.edu>
2023-03-10 14:29:40 -0500

    Added javadoc

--- HW6/src/hw6/simulation/Jukebox.java
+++ HW6/src/hw6/simulation/Jukebox.java
@@ -13,6 +13,16 @@ import java.io.IOException;
 import java.util.*;
 
 
+/**
+ * The main program for the jukebox simulation
+ * <pre>
+ * $ java Jukebox filename seed
+ * Usage: java Jukebox filename seed
+ *</pre>
+ *
+ * @author Jordan Chin, jc9627@rit.edu
+ * @author Charlie Leyens, cal3368@rit.edu
+ */
 public class Jukebox {
 
     /** The seed to for the random number generator */
