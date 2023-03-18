0205edaa90d869071aa5a4fcafc600d352cfb2d2
jyc119 <1jordanchin372@gmail.com>
2023-03-18 12:00:30 -0400

    Created song class and able to read files

--- HW6/src/hw6/simulation/Jukebox.java
+++ HW6/src/hw6/simulation/Jukebox.java
@@ -25,4 +25,41 @@ import java.util.*;
  */
 public class Jukebox {
 
+    public static List<Song> getSongs(String filename){
+        BufferedReader reader;
+        List<Song> songs = new ArrayList<>();
+        try {
+            reader = new BufferedReader(new FileReader(filename));
+            String line = reader.readLine();
+            while (line != null) {
+                String[] arrOfStr = line.split("<SEP>", 4);
+                songs.add(new Song(arrOfStr[3],arrOfStr[2]));
+                // read next line
+                line = reader.readLine();
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return songs;
+    }
+
+    public static void openingLine(int songs){
+        System.out.println("Jukebox of " + songs + " songs starts rockin'...");
+        System.out.println("Printing first 5 songs played...");
+    }
+
+    public static void main(String[] args){
+        if (args.length != 1) {
+            System.out.println("Usage: java HeroStorm " +
+                    "dragon_seed_# lion_seed_#");
+        } else {
+            String filename = args[0];
+            List<Song> newsongs = new ArrayList<>();
+            newsongs = getSongs(filename);
+            System.out.println(newsongs);
+        }
+    }
+
 }
--- HW6/src/hw6/simulation/Song.java
+++ HW6/src/hw6/simulation/Song.java
@@ -14,6 +14,23 @@ import java.util.Objects;
  * @author Jordan Chin, jc9627@rit.edu
  * @author Charlie Leyens, cal3368@rit.edu
  */
-public class Song {
+public class Song implements Comparable<Song> {
 
+    private String name;
+    private String artist;
+
+    public Song(String name, String artist){
+        this.name = name;
+        this.artist = artist;
+    }
+
+    public int hashcode(){return this.name.hashCode() + this.artist.hashCode();}
+    public Boolean equals(Song song){return song.name == this.name && song.artist == this.artist;}
+
+    public String toString(){return "Artist: " + this.artist + ", Song: " + this.name;}
+
+    @Override
+    public int compareTo(Song o) {
+        return 0;
+    }
 }
