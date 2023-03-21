ad4467dbea9789933fc94c4bd01a8c05c0dbcc25
jyc119 <1jordanchin372@gmail.com>
2023-03-21 19:27:53 -0400

    Added new constructor to battleshipexception

--- HW7/src/BattleshipException.java
+++ HW7/src/BattleshipException.java
@@ -1,7 +1,9 @@
 public class BattleshipException extends Exception {
 
     public static final int UNSET = -1;
+    public final int row;
 
+    public final int column;
     // Create public integer fields row and column.
     // Make them so they cannot be changed, and non-static.
 
@@ -10,9 +12,17 @@ public class BattleshipException extends Exception {
     //
     public BattleshipException( int row, int column, String message ) {
         super( message + ", row=" + row + ", column=" + column );
+        this.row = row;
+        this.column = column;
     }
 
     // Add a second (overloading) constructor that only takes a
     // message string. It should pass the string up to its superclass
     // and set row and column to UNSET.
+    public BattleshipException(String message){
+        super(message);
+        this.row = UNSET;
+        this.column = UNSET;
+    }
+
 }
